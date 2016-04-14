70032f98a285d7efec9382cc2f7efefe9bb5895c
janek_g <jmgrzesik@gmail.com>
2016-04-14 20:52:05 +0200

    Adds explores simple chat server that bases on events and emitters and es6 features

--- src/simple_chat/index.js
+++ src/simple_chat/index.js
@@ -1,40 +1,118 @@
 /* @flow */
+"use strict";
 
-import {EventEmitter} from "events";
-import net from "net";
+let events = require('events');
+let net = require('net');
+let _ = require('lodash');
+let moment = require('moment');
 
 
-class Channel extends EventEmitter {
+class Channel extends events.EventEmitter {
     constructor() {
         super();
         this.clients = {};
+        this.aliases = {};
         this.subscriptions = {};
+        this.initializeListeners();
+    }
+
+    _createSubscriptionFunction(id) {
+        return (senderId, message)=> {
+            if (id !== senderId) {
+                this.clients[id].consumeMessage(this.formatMessage(senderId, `wrote: ${message}`));
+            }
+        }
+    }
+
+    getAliasForId(id) {
+        let alias = this.aliases[id];
+        if (alias) {
+            return alias;
+        }
+        throw new Error('alias not found');
+    }
+
+    formatMessage(referencedId, message) {
+        return `[${this.getAliasForId(referencedId)}] on ${moment().format('h:mm:ss')} ${message}`
+    }
+
+    initializeListeners() {
+        this.on('join', (id, client)=> {
+            this.clients[id] = client;
+            this.aliases[id] = client.alias;
+            this.announceJoined(id);
+            this.subscriptions[id] = this._createSubscriptionFunction(id);
+            this.on('broadcast', this.subscriptions[id]);
+        });
+    }
+
+    announceJoined(joinedId) {
+        _.forEach(this.clients, (client)=> {
+            client.consumeMessage(this.formatMessage(joinedId, 'joined the channel'));
+        })
+    }
+}
+
+class Client {
+    constructor(alias, connection, channel) {
+        this._connection = connection;
+        this._alias = alias;
+        this._channel = channel;
+        this._id = Client.createId(this._connection);
+        this.joinChannel();
+        this.listenForData();
+    }
+
+    get alias() {
+        return this._alias;
+    }
+
+    joinChannel() {
+        this._channel.emit('join', this._id, this);
+    }
+
+    static createId(connection) {
+        return `${connection.remoteAddress}:${connection.remotePort}`;
+    }
+
+    listenForData() {
+        this._connection.on('data', (data)=> {
+            data = ChatServer.purifyString(data);
+            this._channel.emit('broadcast', this._id, data);
+        });
+    }
+
+    consumeMessage(message) {
+        this._connection.write(`${message}\n`);
+    }
+}
+
+class ChatServer {
+    constructor(channel, port) {
+        this._channel = channel;
+        this.socketServer = this.createSocketServer(port);
+    }
+
+    createSocketServer(port) {
+        return net.createServer((connection)=> {
+            this.connectionListenerFunction(connection)
+        }).listen(port, ()=> {
+            console.log('server is listening');
+        });
+    }
+
+    connectionListenerFunction(connection) {
+        console.log(`new client connected`);
+        connection.write('Please type your alias:\n');
+        connection.once('data', (alias)=> {
+            new Client(ChatServer.purifyString(alias), connection, this._channel);
+        });
+    }
+
+    static purifyString(alias) {
+        return alias.toString().trim();
     }
 }
 
 let channel = new Channel();
-
-channel.on('join', (id, client)=> {
-    this.clients[id] = client;
-    this.subscriptions[id] = createSubscriptionFunction(id);
-    this.on('broadcast', this.subscriptions[id].call(this));
-});
-
-let createSubscriptionFunction = (id)=> {
-    return (senderId, message)=> {
-        if (id !== senderId) {
-            this.clients[id].write(message);
-        }
-    }
-};
-
-let server = net.createServer((client)=> {
-    const id = `{client.remoteAddress}:{client.remotePort}`;
-    client.on('connect', ()=> {
-        channel.emit('join', id, client);
-    });
-    client.on('data', (data)=> {
-        data = data.toString();
-        channel.emit('broadcast', id, data);
-    })
-});
+let server = new ChatServer(channel, 8888);
